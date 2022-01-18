02a1dc81067e5ed0c8f6f795f3e5363c4dad7b64
adamdune <adam.ho.buss@gmail.com>
2022-01-18 13:33:44 +0800

    chore: run prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,15 +19,10 @@ function App() {
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
